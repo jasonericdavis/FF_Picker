494f929f0142a9d382e279ee3b0ab5421049b0d6
Jason Davis <jason@jasonericdavis.dev>
2020-09-21 15:40:13 -0400

    converted to js project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const fs = require('fs');
+const path = require('path')
+
+const app = express();
+const port = 3000;
+
+
+/*  
+*   This middleware goes before the static middleware
+*   Becuase if the file exist then it will send the response and 
+*   any middleware after it will not be executed.
+*/
+app.use(async (req, res, next) => {
+    try {
+        if(req.path.endsWith('.csv')) {
+            const filename = path.join(__dirname,'public',req.path)
+            if(fs.existsSync(filename)){
+                // asynchronously read the file 
+                // the await command causes the program to leave from this 
+                // function until readFile is complete and then it will 
+                // come and finish executing the function
+                await fs.readFile(filename, 'utf8', (err, data) => {
+                    res.send(data)
+                })
+                return
+            }                
+        }
+        next()
+
+    } catch(error) {
+        next(error)
+    }
+})
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`App started on port: ${port}`)
+})
