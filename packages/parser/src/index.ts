b2fed88f3592e9ccd8f21f77dec51180564186bf
Jason Davis <jason@jasonericdavis.dev>
2020-11-26 09:22:10 -0500

    checking in because vs code insiders build stopped working

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -4,20 +4,20 @@ import {getData} from './getPFParseData'
 
 let args = process.argv
 console.log(args)
-const folder = path.join(process.cwd(),'data', '2020-8')
+const folder = path.join(process.cwd(),'data', '2020-10')
 
 getData(folder, path.join(process.cwd(),'data', 'pfSchedule.csv'))
     .then(({players, teams, games, units}) => {
         console.log(`creating data files`)
-        fs.writeFile(path.join(__dirname, '../public/players.json'), JSON.stringify(players), (error) => {
+        fs.writeFile(path.join(folder, 'players.json'), JSON.stringify(players), (error) => {
             if(error) console.log(`there was an error writing to file: ${error}`)
         })
 
-        fs.writeFile(path.join(__dirname, '../public/teams.json'), JSON.stringify(teams), (error) => {
+        fs.writeFile(path.join(folder, 'teams.json'), JSON.stringify(teams), (error) => {
             if(error) console.log(`there was an error writing to file: ${error}`)
         })
 
-        fs.writeFile(path.join(__dirname, '../public/games.json'), JSON.stringify(games), (error) => {
+        fs.writeFile(path.join(folder, 'games.json'), JSON.stringify(games), (error) => {
             if(error) console.log(`there was an error writing to file: ${error}`)
         })
 
