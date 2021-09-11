e6836000559c38e4ec1f844c3e3b8ba49e79c153
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-09-11 10:42:39 -0400

    resolves #20 and #21 added player and defensive stats to

--- actions/data-grabber.js
+++ actions/data-grabber.js
@@ -1,15 +1,30 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({headless: true});
+const downloadData = async (url, elementId) => {
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto('https://www.pro-football-reference.com/years/2020/#');
+    await page.goto(url);
 
-    const teamStats = await page.evaluate(() => {
-        table2csv("team_stats");
-        return document.getElementById('csv_team_stats').innerText;
-    });       
-    
-    console.log(teamStats);
+    const data = await page.evaluate(elementId => {
+        console.log(`Inner Text is ${elementId}`)
+        table2csv(elementId);
+        return document.getElementById(`csv_${elementId}`).innerText;
+    }, elementId)
+
     await browser.close();
+    return data;
+}
+
+(async () => {
+    const offensiveStats = await downloadData(
+        'https://www.pro-football-reference.com/years/2020/#', 'team_stats'
+    );
+
+    const playerStats = await downloadData(
+        'https://www.pro-football-reference.com/years/2020/fantasy.htm', 'fantasy'
+    );
+
+    const defensiveStats = await downloadData(
+        'https://www.pro-football-reference.com/years/2020/opp.htm', 'team_stats'
+    );
 })();
