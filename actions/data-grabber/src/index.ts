12abbfdaeba9177da2ec92b13c1bc0d56041bb1f
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-11-26 11:14:34 -0500

    code cleanup

--- actions/data-grabber/src/index.ts
+++ actions/data-grabber/src/index.ts
@@ -1,30 +1,11 @@
-//import fs from 'fs';
-import path from 'path';
-// import  parsePlayerData from './parsePlayerData';
-// import parseOffensiveData from './parseOffensiveData';
-// import parseDefsiveData from './parseDefensiveData';
-//import getUnits from './getUnits';
 import { getPreviousWeeksSchedule } from './getSchedule';
-//import parseGameData from './parseGameData';
 import { uploadTeamsToSupabase, uploadPlayersToSupabase, uploadCache } from './uploadToSupabase';
-// import {teams as teamsCache} from './teams';
 import getStats from './getStats';
 import parseData from './parseData';
 import cacheStats from './cacheStats';
 
 async function execute() {
-    // const offensiveStats = await downloadData(
-    //     `${baseUrl}#`, 'team_stats'
-    // );
-    // fs.writeFileSync(path.join(cacheDir, 'offensive-stats.csv'), offensiveStats);
-    // const offenses = parseOffensiveData(offensiveStats);
-
-    // const defensiveStats = await downloadData(
-    //     `${baseUrl}opp.htm`, 'team_stats'
-    // );
-    // fs.writeFileSync(path.join(cacheDir,'defensive-stats.csv'), defensiveStats);
-    //const defenses = parseDefsiveData(defensiveStats);
-
+    console.log('Downloading schedule...');
     const scheduledGames = await getPreviousWeeksSchedule();
     const previousWeek = scheduledGames.length > 0 ? scheduledGames[0].week : 0;
     console.log(`Processing Week: ${previousWeek}`);
@@ -38,23 +19,11 @@ async function execute() {
         offensiveStats, defensiveStats, playerStats, teams
     )
 
-    //const teams = createTeams(previousWeek, offenses, defenses);
-    
-    // fs.writeFileSync(path.join(cacheDir,'teams.json'), JSON.stringify(teams));
-
-    // const playerStats = await downloadData(
-    //     `${baseUrl}fantasy.htm`, 'fantasy'
-    // );
-    // fs.writeFileSync(path.join(cacheDir,'player-stats.csv'), playerStats);
-    // const players = parsePlayerData(playerStats, teamsCache);
-
-    
+    console.log('Uploading to Supabase...');
     uploadTeamsToSupabase(teams);
     uploadPlayersToSupabase(players, previousWeek);
     uploadCache(previousWeek, cache);
-    // uploadFileToStorage(path.join(cacheDir, 'offensive-stats.csv'), previousWeek)
-    // uploadFileToStorage(path.join(cacheDir, 'defensive-stats.csv'), previousWeek)
-    // uploadFileToStorage(path.join(cacheDir, 'player-stats.csv'), previousWeek)
+    console.log('Done!');
 };
 
 execute();
--- actions/data-grabber/src/parseData.ts
+++ actions/data-grabber/src/parseData.ts
@@ -1,7 +1,31 @@
-import { createStatPointer } from './util'
-import {Offense, Defense, Player,Players, Team} from './types'
+import {Offense, Defense, Player,Team} from './types'
 import nicknames from './nicknames'
 import {teams as teamsCache} from './teams';
+
+/**
+ * This function takes a array of statHeaders and returns them as an object with the 
+ * position of the stat in the staHeaders array
+ * @param statHeaders array of stat headers that are to be used
+ * @returns An object with the key of the stat header and its position in the header array
+ */
+  function createStatPointer(statHeaders: Array<string>) {
+    let statPtr: {[key:string]: number} = {}
+    statHeaders.map((col, colIndex) => {
+        let tempColName = col
+        let colNameCounter = 0
+
+        /**  
+         * Because the name of a column can appear multiple times in the list of columns
+         * this logic will append a suffix to the column if it is already in the list of columns
+         * */ 
+        while(statPtr[`${tempColName}`]) {
+            colNameCounter += 1
+            tempColName = `${tempColName.replace(`_${colNameCounter - 1}`, '')}_${colNameCounter}`             
+        } 
+        statPtr[tempColName ] = colIndex
+    })
+    return statPtr
+}
 
 function createOffenseFromStats(stats: string[], statPtr: {[key:string]:number}): Offense {
     return {
